190795988e2ee976919196165369d162042e80a5
HassenAhmedAbdi <7assenmhd@gmail.com>
2026-06-17 12:47:58 +0000

    initial

---
+++ web-tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace web_tests;
+
+public class RootIntegration
+{
+    [Fact]
+    public async Task Test1Async()
+    {
+
+		//arrange
+		HttpClient client=new();
+
+		//act
+		var result=await client.GetAsync("http://localhost:8080/");
+
+
+		//assert
+		Assert.True(result.IsSuccessStatusCode);
+		Assert.Equal("Hello World!",await result.Content.ReadAsStringAsync(default));
+    }
+}
